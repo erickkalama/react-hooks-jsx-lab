528c872ef659648ff146a68c7b074507ca0ac3a8
GHOSTFACE <erickkatana4787@gmail.com>
2024-08-20 19:08:29 +0300

    final updated files

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,17 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>
+        Hello! I'm a passionate developer who enjoys building web applications.
+        I love exploring new technologies and constantly learning to improve my skills.
+      </p>
+      <img src={image} alt="I made this" />
+    </div>
+  );
 }
+
 
 export default About;
